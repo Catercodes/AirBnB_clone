68ed6d0823531eaece47514a8bb9e374ec4d1664
Catercodes <chrisadamsadewuni@gmail.com>
2024-04-16 07:19:37 +0100

    Store first object

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,32 +1,40 @@
 #!/usr/bin/python3
-""" This module contains  Filestorage"""
+"""
+Serializes instances to a JSON file and deserializes JSON file to instances
+"""
 import json
-import os
-from models.base_model import BaseModel
+from datetime import datetime
+# from models.user import User
+# from models.state import State
+# from models.city import City
+# from models.amenity import Amenity
+# from models.place import Place
+# from models.review import Review
 
 
 class FileStorage:
-    """The Private class attribute"""
-    __file_path = "file.json"
+    """
+    Serializes instances to a JSON file and deserializes JSON file to instances
+    """
+    __file_path = 'file.json'
     __objects = {}
 
     def all(self):
-        "return  the  dict of the class attr '__object' """
         return FileStorage.__objects
 
     def new(self, obj):
-        """ sets in the __object with the key to obj"""
         key = type(obj).__name__ + '.' + obj.id
         FileStorage.__objects[key] = obj
 
     def save(self):
-        ''' This method save the convert the __object into
-        a json string'''
-        _dict = {}
-        with open(FileStorage.__file_path, 'w',) as json_file:
+        """
+        serializes FileStroage.__objects
+        """
+        with open(FileStorage.__file_path, 'w+') as file_json:
+            dictofobjs = {}
             for key, value in FileStorage.__objects.items():
-                _dict[key] = value.to_dict()
-                json.dump(_dict, json_file)
+                dictofobjs[key] = value.to_dict()
+            json.dump(dictofobjs, file_json)
 
     def reload(self):
         """
@@ -34,13 +42,24 @@ class FileStorage:
         """
         try:
             with open(FileStorage.__file_path, 'r') as file_json:
-                # _dict = json.load(file_json)
-                # _dict = json.loads(file_json.read())
-                data = json.load(file_json)
+                dictofobjs = json.loads(file_json.read())
                 from models.base_model import BaseModel
-                for key, value in data.items():
+                # from models.user import User
+                for key, value in dictofobjs.items():
                     if value['__class__'] == 'BaseModel':
                         FileStorage.__objects[key] = BaseModel(**value)
+                    elif value['__class__'] == 'User':
+                        FileStorage.__objects[key] = User(**value)
+                    elif value['__class__'] == 'Place':
+                        FileStorage.__objects[key] = Place(**value)
+                    elif value['__class__'] == 'State':
+                        FileStorage.__objects[key] = State(**value)
+                    elif value['__class__'] == 'City':
+                        FileStorage.__objects[key] = City(**value)
+                    elif value['__class__'] == 'Amenity':
+                        FileStorage.__objects[key] = Amenity(**value)
+                    elif value['__class__'] == 'Review':
+                        FileStorage.__objects[key] = Review(**value)
 
         except FileNotFoundError:
             pass
