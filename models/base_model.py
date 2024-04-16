07ade98ee57a57062ada9f29309fdfba8cda4b2e
Catercodes <chrisadamsadewuni@gmail.com>
2024-04-16 07:35:51 +0100

    Store first object

--- models/base_model.py
+++ models/base_model.py
@@ -7,9 +7,9 @@ from datetime import time
 class BaseModel:
     def __init__(self, *args, **kwargs):
         """Assigning them to the right values"""
-        #self.id = str(uuid.uuid4())
-        #self.created_at = datetime.now()
-        #self.updated_at = datetime.now()
+        # self.id = str(uuid.uuid4())
+        # self.created_at = datetime.now()
+        # self.updated_at = datetime.now()
         if kwargs:
             for key, value in kwargs.items():
                 if key != '__class__':
@@ -27,11 +27,12 @@ class BaseModel:
 
             storage.new(self)
 
-
     def __str__(self):
         """ returns the string format of the above"""
         _str = "[{:s}] ({:s}) {:s}"
-        _str = _str.format(self.__class__.__name__, self.id, str(self.__dict__))
+        _str = _str.format(
+            self.__class__.__name__, self.id, str(
+                self.__dict__))
         return _str
 
     def save(self):
@@ -42,8 +43,8 @@ class BaseModel:
 
     def to_dict(self):
         """ return the dict  format for  the above """
-        dcopy=self.__dict__.copy()
-        dcopy["__class__"]=self.__class__.__name__
-        dcopy["created_at"]=self.created_at.isoformat()
-        dcopy["updated_at"]=self.updated_at.isoformat()
+        dcopy = self.__dict__.copy()
+        dcopy["__class__"] = self.__class__.__name__
+        dcopy["created_at"] = self.created_at.isoformat()
+        dcopy["updated_at"] = self.updated_at.isoformat()
         return dcopy
